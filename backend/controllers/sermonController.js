e5e13f7c6a83487b00df21d638e6bfb05877f6c1
joseph skokan <joeskokan20@gmail.com>
2025-01-05 01:06:31 -0500

    adding serm

---
+++ backend/controllers/sermonController.js
@@ -0,0 +1,9 @@
+const Sermons = require("../models/sermons");
+
+const SermonController = {
+  createSermon: async (req, res) => {
+    const {} = req.body;
+  },
+};
+
+module.exports = Sermons;
---
+++ backend/models/sermons.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Sermon = sequelize.define("Sermon", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  preacher: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  videoUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  audioUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  transcript: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+});
+
+module.exports = Sermon;
